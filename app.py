ec57215784af6f94f82d5181cd1d112a8c65f975
Jake Hatcher <jake.htchr@gmail.com>
2020-01-28 15:35:50 -0500

    Initial Commit

---
+++ app.py
@@ -0,0 +1,196 @@
+# import necessary libraries
+from flask import (
+    Flask,
+    render_template,
+    jsonify,
+    request)
+import math
+
+
+app = Flask(__name__)
+
+# Create a list to hold our player data
+player_data = {}
+
+@app.route("/")
+def home():
+    # read from file
+    #tfile = open("TempText.txt","r")
+    players = []
+    # return mulitple Lines
+    for key in player_data:
+        players.append(key)
+
+    return jsonify(player_data)
+    
+    #Catch Return Placeholder that should never be reached
+    return "Welcome!"
+
+
+@app.route("/execute_file/<string:tfile>")
+def execute(tfile):
+    output = ""
+    ofile = open(tfile,"r")
+    steps=ofile.readlines()
+    for line in steps:
+        try:
+            path = line.split(":")[0]
+            inputs = line.split(":")[1].strip()
+            if(path == "movePlayer"):
+                move(inputs)
+            elif(path == "turnPlayer"):
+                turn(inputs)
+            elif(path == "mineItem"):
+                mine(inputs)
+            elif(path == "lookupInventory"):
+                output = output + checkInv(inputs)+"\n"
+            elif(path == "lookupItemOwners"):
+                output = output + findOwners(inputs)+"\n"
+            elif(path == "lookupDistance"):
+                output = output + findDistance(inputs)+"\n"
+        except:
+            pass
+
+        #will write over file
+    #writeout = open("output.txt","w")
+        #appends to file
+    writeout = open("output.txt","a")
+    writeout.write(output)
+    writeout.close()
+    return output
+
+
+@app.route("/movePlayer/<string:input>")
+def move(input):
+    try:
+        player = input.split(",")[0]
+        distance = int(input.split(",")[1])
+    except:
+        return "Error"
+
+    # Moves assuming north is positive x and east is positive y
+    if player not in player_data:
+        player_data[player] = {"name":player,"location":[0,0],"direction":"north","inventory":[]}
+
+    if(player_data[player]["direction"] == "north"):
+        player_data[player]["location"] = [player_data[player]["location"][0] + distance,player_data[player]["location"][1]]
+    elif(player_data[player]["direction"] == "east"):
+        player_data[player]["location"] = [player_data[player]["location"][0] ,player_data[player]["location"][1]+ distance]
+    elif(player_data[player]["direction"] == "south"):
+        player_data[player]["location"] = [player_data[player]["location"][0] - distance,player_data[player]["location"][1]]
+    else: #west
+        player_data[player]["location"] = [player_data[player]["location"][0] ,player_data[player]["location"][1]- distance]
+
+    return home()
+
+@app.route("/turnPlayer/<string:input>")
+def input(input):
+    try:
+        player = input.split(",")[0]
+        direction =input.split(",")[1]
+    except:
+        return "Error"
+
+    #checks for invalid directions
+    if direction.lower() not in ["left","right"]:
+        return "Error: Invalid Direction"
+
+    if player not in player_data:
+        player_data[player] = {"name":player,"location":[0,0],"direction":"north","inventory":[]}
+
+    if(player_data[player]["direction"] == "north"):
+        player_data[player]["direction"] = "west" if direction.lower()=="left" else "east"
+    elif(player_data[player]["direction"] == "east"):
+        player_data[player]["direction"] = "north" if direction.lower()=="left" else "south"
+    elif(player_data[player]["direction"] == "south"):
+        player_data[player]["direction"] = "east" if direction.lower()=="left" else "west"
+    else: #west
+        player_data[player]["direction"] = "south" if direction.lower()=="left" else "north"
+
+    return home()
+
+@app.route("/mineItem/<string:input>")
+def mine(input):
+    try:
+        player = input.split(",")[0]
+        item =input.split(",")[1]
+    except:
+        return "Error"
+
+    if player not in player_data:
+        player_data[player] = {"name":player,"location":[0,0],"direction":"north","inventory":[]}
+
+    player_data[player]["inventory"].append(item)
+
+    return home()
+
+@app.route("/lookupInventory/<string:player>")
+def checkInv(player):
+    
+    if player not in player_data:
+        return "Error: Player does not exist"
+
+    inventory  = {}
+    playerInv = player_data[player]["inventory"]
+
+    for item in playerInv:
+        if item in inventory:
+            inventory[item] = inventory[item]+1
+        else:
+            inventory[item] = 1
+
+    invOutput = ""
+    for key in inventory:
+        invOutput = invOutput + key +":"+str(inventory[key])+","
+    invOutput = invOutput[:-1]
+
+    return invOutput
+
+@app.route("/lookupItemOwners/<string:item>")
+def findOwners(item):
+    
+    players  = []
+    
+    for key in player_data:
+        if item in player_data[key]["inventory"]:
+            players.append(key)
+    if (len(players)<1):
+        return "No Players have this item"
+
+    outputs = ""
+    for player in players:
+        outputs = outputs + player +","
+    return outputs[:-1]
+
+@app.route("/lookupDistance/<string:input>")
+def findDistance(input):
+    
+    #Looks for two players
+    if(len(input.split(","))==2):
+        player = input.split(",")[0]
+        player2 = input.split(",")[1]
+        if((player not in player_data)or(player2 not in player_data)):
+            return "Error: One or more invalid players given"
+        return getDistance(player_data[player]["location"][0],player_data[player]["location"][1],player_data[player2]["location"][0],player_data[player2]["location"][1])
+
+    #Loods for 1 player and 2 valid cordinates
+    elif(len(input.split(","))==3):
+        try:
+            player = input.split(",")[0]
+            x = int(input.split(",")[1])
+            y = int(input.split(",")[2])
+        except:
+            return "Error: Invalid coordinates"
+        if(player not in player_data):
+            return "Error: Invalid player given"
+        return getDistance(player_data[player]["location"][0],player_data[player]["location"][1],x,y)
+
+    return "Error: Incorrect inputs"
+
+
+#Math Happens
+def getDistance(x1,y1,x2,y2):
+    return str(math.sqrt(((x2-x1)*(x2-x1))+((y2-y1)*(y2-y1))))
+
+if __name__ == "__main__":
+    app.run(debug=True)
